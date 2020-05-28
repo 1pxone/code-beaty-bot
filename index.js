ff0ac817d2c166822fa985e368a3daf59b043b3c
Artemiy Vereshchinskiy <a.vereshchinskiy@netris.ru>
2020-05-29 01:50:56 +0300

    feat: bot born

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const Telegraf = require('telegraf');
-const API_TOKEN = process.env.API_TOKEN || '';
+const BOT_TOKEN = process.env.BOT_TOKEN || '';
 const PORT = process.env.PORT || 3000;
 const URL = process.env.URL;
-const bot = new Telegraf(API_TOKEN);
-
-bot.telegram.setWebhook(`${URL}/bot${API_TOKEN}`);
-bot.startWebhook(`/bot${API_TOKEN}`, null, PORT)
-
+const bot = new Telegraf(BOT_TOKEN);
+bot.telegram.setWebhook(`${URL}/bot${BOT_TOKEN}`);
+bot.startWebhook(`/bot${BOT_TOKEN}`, null, PORT)
 bot.command('oldschool', (ctx) => ctx.reply('Hello'));
 bot.command('modern', ({ reply }) => reply('Yo'));
 bot.command('hipster', Telegraf.reply('λ'));
